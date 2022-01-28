8480a135a5f0f0cae3becb010ce1dec0e7b97aab
Yash Rawat <rawatyash1999@gmail.com>
2022-01-28 20:47:08 +0530

    chore: added handlebars and morgan to the app

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 dotenv.config({
   path: './config/config.env',
 });
+
 connectDB();
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 const PORT = process.env.PORT || 5000;
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 app.listen(
   PORT,
   console.log(`Server running on ${process.env.NODE_ENV} mode on port: ${PORT}`)
