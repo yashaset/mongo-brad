e085d004d9f8be9d3b7d0f164b5c27512191acf0
Yash Rawat <rawatyash1999@gmail.com>
2022-01-28 20:42:25 +0530

    feat: connect to mongo db cluster

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config({
   path: './config/config.env',
 });
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(
